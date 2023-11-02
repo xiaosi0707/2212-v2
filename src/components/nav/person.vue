33239019e338fe186ceea48a4aa276bf15ea7160
zuibiancheng <zuibiancheng@foxmail.com>
2023-11-02 15:23:03 +0800

    路由独享守卫

---
+++ src/components/nav/person.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>欢迎尊贵的VIP客户</h1>
+</template>
--- src/router/index-nav.js
+++ src/router/index-nav.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 import Login from '../components/nav/login.vue'
 import Main from '../components/nav/main.vue'
+import Person from '../components/nav/person.vue'
 
 //3．创建路由的实例对象
 const router = new VueRouter({
@@ -20,38 +21,21 @@ const router = new VueRouter({
       meta: {
         title: '欢迎进入后台管理系统主页'
       }
+    },
+    {
+      path: '/person',
+      component: Person,
+      meta: {
+        vip: !true
+      },
+      beforeEnter: (to, from, next) => {
+        if(to.meta.vip) {
+          next()
+        } else {
+          next('/')
+        }
+      }
     }
   ],
 });
-// 路由前置守卫
-router.beforeEach((to, from ,next) => {
-  console.log('beforeEach')
-  if(to.path === '/main') {
-    // 你想进入后台管理系统那么我就得检查你有没有权限（在线状态就有权限反之没有权限）
-    const token = localStorage.getItem('token')
-    // 如果token值存在就证明你小子在线就可以正常放行
-    if (token) {
-      next()
-    } else {
-      // 你小子不在线就不能放行
-      next('/')
-    }
-  } else {
-    // 放行
-    next()
-  }
-})
-router.beforeResolve((to, from, next) => {
-  console.log('beforeResolve')
-  next()
-})
-// 路由后置守卫
-router.afterEach((to, from) => {
-  console.log(to, from)
-  if (to.path === '/') {
-    document.title = to.meta.title
-  } else if (to.path === '/main') {
-    document.title = to.meta.title
-  }
-})
 export default router;
