f78a525b96ff568f185256efe959598ade2e5ef9
zuibiancheng <zuibiancheng@foxmail.com>
2023-11-14 10:10:54 +0800

    ''

--- src/views/goods-add.vue
+++ src/views/goods-add.vue
@@ -1,5 +1,332 @@
 <template>
-	<div>
-		添加商品组件
-	</div>
+  <div>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品添加</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 面包屑 -->
+    <!-- 表格 -->
+    <el-card class="box-card">
+      <!-- 提示 -->
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
+      </el-alert>
+      <!-- 提示 -->
+      <!-- 步骤条 -->
+      <el-steps :active="active" finish-status="success" class="wyf-steps" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+      <!-- 步骤条 -->
+      <!-- 标签页 -->
+      <el-tabs :tab-position="tabPosition">
+        <el-tab-pane label="基本信息">
+          <el-form label-position="top" label-width="80px">
+            <el-form-item label="商品名称">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类">
+              <el-cascader v-model="value" :options="options" :props="{ expandTrigger: 'hover' }" @change="handleChange"></el-cascader>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="商品参数">
+          <el-form label-position="top" label-width="80px">
+            <el-form-item label="版式">
+              <el-checkbox v-model="checked1" label="备选项1" border></el-checkbox>
+              <el-checkbox v-model="checked2" label="备选项2" border></el-checkbox>
+            </el-form-item>
+            <el-form-item label="内存">
+              <el-checkbox v-model="checked1" label="备选项1" border></el-checkbox>
+            </el-form-item>
+            <el-form-item label="CPU主频">
+              <el-checkbox v-model="checked1" label="备选项1" border></el-checkbox>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="商品属性">
+          <el-form label-position="top" label-width="80px">
+            <el-form-item label="商品名称">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量">
+              <el-input></el-input>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="商品图片">
+          <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList" list-type="picture">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-tab-pane>
+        <el-tab-pane label="商品内容">
+          <quill-editor></quill-editor>
+        </el-tab-pane>
+      </el-tabs>
+      <!-- 标签页 -->
+    </el-card>
+    </el-card>
+    <!-- 表格 -->
+  </div>
 </template>
+<script>
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+export default {
+  components: { quillEditor },
+  data() {
+    return {
+      active: 0,
+      // 标签页的位置（上下左右）
+      tabPosition: 'left',
+      // 上传图片
+      fileList: [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }, { name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }],
+      /* 商品基本信息表单数据 */
+      /* 商品基本信息表单数据 */
+
+      /*级联选择器*/
+      value: [],
+      options: [{
+        value: 'zhinan',
+        label: '指南',
+        children: [{
+          value: 'shejiyuanze',
+          label: '设计原则',
+          children: [{
+            value: 'yizhi',
+            label: '一致'
+          }, {
+            value: 'fankui',
+            label: '反馈'
+          }, {
+            value: 'xiaolv',
+            label: '效率'
+          }, {
+            value: 'kekong',
+            label: '可控'
+          }]
+        }, {
+          value: 'daohang',
+          label: '导航',
+          children: [{
+            value: 'cexiangdaohang',
+            label: '侧向导航'
+          }, {
+            value: 'dingbudaohang',
+            label: '顶部导航'
+          }]
+        }]
+      }, {
+        value: 'zujian',
+        label: '组件',
+        children: [{
+          value: 'basic',
+          label: 'Basic',
+          children: [{
+            value: 'layout',
+            label: 'Layout 布局'
+          }, {
+            value: 'color',
+            label: 'Color 色彩'
+          }, {
+            value: 'typography',
+            label: 'Typography 字体'
+          }, {
+            value: 'icon',
+            label: 'Icon 图标'
+          }, {
+            value: 'button',
+            label: 'Button 按钮'
+          }]
+        }, {
+          value: 'form',
+          label: 'Form',
+          children: [{
+            value: 'radio',
+            label: 'Radio 单选框'
+          }, {
+            value: 'checkbox',
+            label: 'Checkbox 多选框'
+          }, {
+            value: 'input',
+            label: 'Input 输入框'
+          }, {
+            value: 'input-number',
+            label: 'InputNumber 计数器'
+          }, {
+            value: 'select',
+            label: 'Select 选择器'
+          }, {
+            value: 'cascader',
+            label: 'Cascader 级联选择器'
+          }, {
+            value: 'switch',
+            label: 'Switch 开关'
+          }, {
+            value: 'slider',
+            label: 'Slider 滑块'
+          }, {
+            value: 'time-picker',
+            label: 'TimePicker 时间选择器'
+          }, {
+            value: 'date-picker',
+            label: 'DatePicker 日期选择器'
+          }, {
+            value: 'datetime-picker',
+            label: 'DateTimePicker 日期时间选择器'
+          }, {
+            value: 'upload',
+            label: 'Upload 上传'
+          }, {
+            value: 'rate',
+            label: 'Rate 评分'
+          }, {
+            value: 'form',
+            label: 'Form 表单'
+          }]
+        }, {
+          value: 'data',
+          label: 'Data',
+          children: [{
+            value: 'table',
+            label: 'Table 表格'
+          }, {
+            value: 'tag',
+            label: 'Tag 标签'
+          }, {
+            value: 'progress',
+            label: 'Progress 进度条'
+          }, {
+            value: 'tree',
+            label: 'Tree 树形控件'
+          }, {
+            value: 'pagination',
+            label: 'Pagination 分页'
+          }, {
+            value: 'badge',
+            label: 'Badge 标记'
+          }]
+        }, {
+          value: 'notice',
+          label: 'Notice',
+          children: [{
+            value: 'alert',
+            label: 'Alert 警告'
+          }, {
+            value: 'loading',
+            label: 'Loading 加载'
+          }, {
+            value: 'message',
+            label: 'Message 消息提示'
+          }, {
+            value: 'message-box',
+            label: 'MessageBox 弹框'
+          }, {
+            value: 'notification',
+            label: 'Notification 通知'
+          }]
+        }, {
+          value: 'navigation',
+          label: 'Navigation',
+          children: [{
+            value: 'menu',
+            label: 'NavMenu 导航菜单'
+          }, {
+            value: 'tabs',
+            label: 'Tabs 标签页'
+          }, {
+            value: 'breadcrumb',
+            label: 'Breadcrumb 面包屑'
+          }, {
+            value: 'dropdown',
+            label: 'Dropdown 下拉菜单'
+          }, {
+            value: 'steps',
+            label: 'Steps 步骤条'
+          }]
+        }, {
+          value: 'others',
+          label: 'Others',
+          children: [{
+            value: 'dialog',
+            label: 'Dialog 对话框'
+          }, {
+            value: 'tooltip',
+            label: 'Tooltip 文字提示'
+          }, {
+            value: 'popover',
+            label: 'Popover 弹出框'
+          }, {
+            value: 'card',
+            label: 'Card 卡片'
+          }, {
+            value: 'carousel',
+            label: 'Carousel 走马灯'
+          }, {
+            value: 'collapse',
+            label: 'Collapse 折叠面板'
+          }]
+        }]
+      }, {
+        value: 'ziyuan',
+        label: '资源',
+        children: [{
+          value: 'axure',
+          label: 'Axure Components'
+        }, {
+          value: 'sketch',
+          label: 'Sketch Templates'
+        }, {
+          value: 'jiaohu',
+          label: '组件交互文档'
+        }]
+      }],
+      checked1: true,
+      checked2: false,
+    };
+  },
+
+  methods: {
+    handleChange(value) {
+      console.log(value);
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    }
+  }
+}
+
+</script>
+<style scoped>
+.wyf-steps {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+</style>
