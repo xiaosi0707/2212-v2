d93a30799c196805e3efd3b4ee094586b4e9b688
zuibiancheng <zuibiancheng@foxmail.com>
2024-05-15 15:39:19 +0800

    28

--- src/views/login.vue
+++ src/views/login.vue
@@ -7,7 +7,7 @@
       <el-input v-model="form.password"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" class="wyf-btn-login" @click="login">登27陆</el-button>
+      <el-button type="primary" class="wyf-btn-login" @click="login">登28陆</el-button>
     </el-form-item>
   </el-form>
 </template>
